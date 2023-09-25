5a901d68e14476de0a899f6fb1030c335ad12355
Nanguage <Nanguage@yahoo.com>
2023-09-25 11:54:18 +0800

    add inference plugin

--- src/napari_ufish/_widget.py
+++ src/napari_ufish/_widget.py
@@ -6,41 +6,65 @@ see: https://napari.org/stable/plugins/guides.html?#widgets
 
 Replace code below according to your needs.
 """
-from typing import TYPE_CHECKING
-
-from magicgui import magic_factory
-from qtpy.QtWidgets import QHBoxLayout, QPushButton, QWidget
-
-if TYPE_CHECKING:
-    import napari
+from qtpy import QtWidgets
+from ufish.api import UFish
+import napari
 
 
-class ExampleQWidget(QWidget):
-    # your QWidget.__init__ can optionally request the napari viewer instance
-    # in one of two ways:
-    # 1. use a parameter called `napari_viewer`, as done here
-    # 2. use a type annotation of 'napari.viewer.Viewer' for any parameter
-    def __init__(self, napari_viewer):
+class InferenceWidget(QtWidgets.QWidget):
+    def __init__(self, napari_viewer: "napari.Viewer"):
         super().__init__()
         self.viewer = napari_viewer
+        self.viewer.layers.events.inserted.connect(
+            lambda e: self._update_layer_select())
+        self.viewer.layers.events.removed.connect(
+            lambda e: self._update_layer_select())
+        self.viewer.layers.events.moved.connect(
+            lambda e: self._update_layer_select())
 
-        btn = QPushButton("Click me!")
-        btn.clicked.connect(self._on_click)
+        btn = QtWidgets.QPushButton("Run")
+        btn.clicked.connect(self._on_run_click)
 
-        self.setLayout(QHBoxLayout())
-        self.layout().addWidget(btn)
+        select_line = QtWidgets.QHBoxLayout()
+        select_line.addWidget(QtWidgets.QLabel("Select input:"))
+        self.layer_select = QtWidgets.QComboBox()
+        self._update_layer_select()
+        self.layer_select.activated.connect(self._update_layer_select)
+        select_line.addWidget(self.layer_select)
 
-    def _on_click(self):
-        print("napari has", len(self.viewer.layers), "layers")
+        layout = QtWidgets.QVBoxLayout()
+        self.setLayout(layout)
+        layout.addLayout(select_line)
+        layout.addWidget(btn)
 
+        self.ufish = UFish()
+        self.ufish.load_weights()
 
-@magic_factory
-def example_magic_widget(img_layer: "napari.layers.Image"):
-    print(f"you have selected {img_layer}")
+    def _on_run_click(self):
+        image_layers = [
+            layer
+            for layer in self.viewer.layers
+            if isinstance(layer, napari.layers.Image)
+        ]
+        if len(image_layers) > 0:
+            idx = self.layer_select.currentIndex()
+            layer = image_layers[idx]
+            print("Run inference on", layer.name)
+            spots, enh_img = self.ufish.predict(layer.data)
 
+            self.viewer.add_image(
+                enh_img, name=f"{layer.name}.enhanced")
+            self.viewer.add_points(
+                spots, name=f"{layer.name}.spots",
+                size=5, opacity=0.5)
 
-# Uses the `autogenerate: true` flag in the plugin manifest
-# to indicate it should be wrapped as a magicgui to autogenerate
-# a widget.
-def example_function_widget(img_layer: "napari.layers.Image"):
-    print(f"you have selected {img_layer}")
+    def _update_layer_select(self):
+        print("update layer select")
+        self.layer_select.clear()
+        for layer in self.viewer.layers:
+            if isinstance(layer, napari.layers.Image):
+                self.layer_select.addItem(layer.name)
+        self.layer_select.update()
+        n = self.layer_select.count()
+        if n > 0:
+            self.layer_select.setCurrentIndex(n-1)
